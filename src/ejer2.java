797e56eb41a00eff741d2391b3598dd74ddb5f3e
Virginia Ortiz <polgarilla@yahoo.es>
2022-11-22 16:25:31 +0100

    Initial commit

---
+++ src/ejer2.java
@@ -0,0 +1,15 @@
+public class ejer2 {
+    public static void main(String[] args) {
+        //número elegido
+        int num=10;
+        //IVA aplicado
+        double iva= num* 0.21;
+        //operación para obtener el número con el iva
+        double suma= num+iva;
+        //resultado
+        System.out.println("El numero mas el IVA es " + suma);
+
+
+    }
+
+}
---
+++ src/prueba1/ejer1.java
@@ -0,0 +1,18 @@
+package prueba1;
+
+public class ejer1 {
+    public static void main(String[] args) {
+        int numeroentero= 8;
+        System.out.println(numeroentero);
+
+        double numerodecimal= 12.50;
+        System.out.println(numerodecimal);
+
+        String cadenadepalabras="esto es un string";
+        System.out.println(cadenadepalabras);
+
+        boolean soymayor=true;
+        System.out.println(soymayor);
+
+    }
+}
